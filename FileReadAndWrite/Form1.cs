5d3694f5b0e4bd68df1b5f27fe61fdd569e39d86
Hajin Kim <enro302@naver.com>
2019-09-27 10:47:58 +0900

    #2 폼 디자인 by 3206 김하진

--- FileReadAndWrite/Form1.cs
+++ FileReadAndWrite/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace FileReadAndWrite
 {
-    public partial class Form1 : Form
+    public partial class FormFile : Form
     {
-        public Form1()
+        public FormFile()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
